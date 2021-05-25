c99f8df070a950fedd579d53aad3f23c1b6c3e12
Frank_Yesid <fyzapatac@unal.edu.co>
2021-05-25 11:34:05 -0500

    Filter small laplacian

--- gcpds/filters/spatial/small_laplacian.py
+++ gcpds/filters/spatial/small_laplacian.py
@@ -1,4 +1,5 @@
-def function_getMontage(montage):
+
+def function_getMontage(database):
     # Small laplacian
     # Calculates spatial filter matrix for Laplacian derivations.
     ##
@@ -40,78 +41,15 @@ def function_getMontage(montage):
     # Frank Y. Zapata C., Luisa F. Velasquez M.
     # Version 2020
     # ------------------------------------------------------------------------
-    if isinstance(montage, str):  # Predefined layouts
-        if montage == '16ch':
-            temp = [[0, 0, 1, 0, 0],
-                    [0, 1, 1, 1, 0],
-                    [0, 1, 1, 1, 0],
-                    [1, 1, 1, 1, 1],
-                    [0, 1, 1, 1, 0],
-                    [0, 0, 1, 0, 0]]
-        if montage == '22ch':     # Database BCI competition IV dataset 2a.
-            temp = [[0, 0, 0, 1, 0, 0, 0],
-                    [0, 1, 1, 1, 1, 1, 0],
-                    [1, 1, 1, 1, 1, 1, 1],
-                    [0, 1, 1, 1, 1, 1, 0],
-                    [0, 0, 1, 1, 1, 0, 0],
-                    [0, 0, 0, 1, 0, 0, 0]]
-        if montage == '24ch':
-            temp = [[0, 1, 0, 0, 1, 0, 0, 1, 0],
-                    [1, 1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1, 1],
-                    [0, 1, 0, 0, 1, 0, 0, 1, 0]]
-        if montage == '28ch':
-            temp = [[0, 0, 0, 1, 0, 0, 0],
-                    [0, 1, 1, 1, 1, 1, 0],
-                    [1, 1, 1, 1, 1, 1, 1],
-                    [0, 1, 1, 1, 1, 1, 0],
-                    [0, 0, 1, 1, 1, 0, 0],
-                    [0, 0, 0, 1, 0, 0, 0],
-                    [1, 1, 1, 0, 1, 1, 1]]
-        if montage == '30ch':
-            temp = [[0, 0, 0, 1, 1, 1, 0, 0, 0],
-                    [1, 1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1, 1]]
-        if montage == '58ch':
-            temp = [[0, 0, 1, 1, 1, 1, 1, 0, 0],
-                    [1, 1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1, 1],
-                    [1, 1, 1, 1, 1, 1, 1, 1, 1],
-                    [0, 0, 1, 1, 1, 1, 1, 0, 0],
-                    [0, 0, 0, 1, 1, 1, 0, 0, 0]],
-        if montage == '60ch':
-            temp = [[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
-                    [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
-                    [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-                    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-                    [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-                    [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
-                    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0]]
-        if montage == '64ch':  # order of channels of database GIGA_MI_ME
-            temp = [[0, 0, 0, 0, 1, 33, 34, 0, 0, 0, 0],
-                    [0, 0, 0, 2, 3, 37, 36, 35, 0, 0, 0],
-                    [0, 7, 6, 5, 4, 38, 39, 40, 41, 42, 0],
-                    [0, 8, 9, 10, 11, 47, 46, 45, 44, 43, 0],
-                    [0, 15, 14, 13, 12, 48, 49, 50, 51, 52, 0],
-                    [0, 16, 17, 18, 19, 32, 56, 55, 54, 53, 0],
-                    [24, 23, 22, 21, 20, 31, 57, 58, 59, 60, 61],
-                    [0, 0, 0, 25, 26, 30, 63, 62, 0, 0, 0],
-                    [0, 0, 0, 0, 27, 29, 64, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 28, 0, 0, 0, 0, 0]]
-        plot_index = np.where(np.asarray(temp).T == 1)
-        n_rows = np.size(np.asarray(temp), 0)
-        n_cols = np.size(np.asarray(temp), 1)
-    else:  # User-defined layouts in the form of a matrix
-        temp = montage
-        plot_index = np.where(np.asarray(temp).T == 1)
-        n_rows = np.size(np.asarray(temp), 0)
-        n_cols = np.size(np.asarray(temp), 1)
+
+    from .masker import generate_mask
+    channels, montage_name = database.metadata['channel_names'], database.metadata['montage']
+
+    montage = generate_mask(channels, montage_name)
+    temp = montage
+    plot_index = np.where(np.asarray(temp).T == 1)
+    n_rows = np.size(np.asarray(temp), 0)
+    n_cols = np.size(np.asarray(temp), 1)
 
     counter = 1
     temp = np.asarray(temp).T
@@ -175,5 +113,9 @@ def function_getMontage(montage):
     if not positions:
         lap = lap[positions, positions]
 
-    lap = lap.T
-    return lap, plot_index, n_rows, n_cols
+    lap  = lap.T
+    Data = database.get_data()
+    for i,tr in enumerate(Data):
+        Data[i] = tr.dot(lap)
+     
+    return Data
